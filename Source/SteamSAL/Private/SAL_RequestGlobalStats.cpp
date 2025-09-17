de72dbc691bd44b3ec342386b592774882c7acd6
Hakan <hakancopur95@gmail.com>
2025-09-17 13:39:35 +0300

    feat(stats): add Request Global Stats async node

---
+++ Source/SteamSAL/Private/SAL_RequestGlobalStats.cpp
@@ -0,0 +1,58 @@
+// SAL_RequestGlobalStats.cpp
+
+#include "SAL_RequestGlobalStats.h"
+
+USAL_RequestGlobalStats* USAL_RequestGlobalStats::RequestGlobalStats(int32 Days)
+{
+	USAL_RequestGlobalStats* Node = NewObject<USAL_RequestGlobalStats>();
+	Node->RequestedDays = FMath::Max(0, Days); // 0 = all-time per Steam docs
+	return Node;
+}
+
+void USAL_RequestGlobalStats::Activate()
+{
+	if (SteamUserStats() == nullptr)
+	{
+		OnFailure.Broadcast();
+		SetReadyToDestroy();
+		return;
+	}
+
+	SteamAPICall_t Handle = SteamUserStats()->RequestGlobalStats(RequestedDays);
+	if (Handle == k_uAPICallInvalid)
+	{
+		OnFailure.Broadcast();
+		SetReadyToDestroy();
+		return;
+	}
+
+	CallResult.Set(Handle, this, &USAL_RequestGlobalStats::OnGlobalStatsReceived);
+}
+
+void USAL_RequestGlobalStats::OnGlobalStatsReceived(GlobalStatsReceived_t* Callback, bool bIOFailure)
+{
+	bool bOk = !bIOFailure
+		&& Callback
+		&& Callback->m_eResult == k_EResultOK;
+
+	if (bOk && SteamUtils())
+	{
+		AppId_t MyApp = SteamUtils()->GetAppID();
+		if (Callback->m_nGameID != (uint64)MyApp)
+		{
+			bOk = false;
+		}
+	}
+
+	if (bOk)
+	{
+		OnSuccess.Broadcast();
+	}
+	else
+	{
+		OnFailure.Broadcast();
+	}
+
+	SetReadyToDestroy();
+}
+
---
+++ Source/SteamSAL/Public/SAL_RequestGlobalStats.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "SALTypes.h"
+
+THIRD_PARTY_INCLUDES_START
+#include "steam/steam_api.h"
+THIRD_PARTY_INCLUDES_END
+
+#include "SAL_RequestGlobalStats.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FSAL_RequestGlobalStatsSuccess);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FSAL_RequestGlobalStatsFailure);
+
+UCLASS()
+class STEAMSAL_API USAL_RequestGlobalStats : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+public:
+
+	UFUNCTION(BlueprintCallable, Category="SteamSAL|Stats",
+		meta=(BlueprintInternalUseOnly="true",
+			  DisplayName="Request Global Stats",
+			  ToolTip="Requests global aggregated stats from Steam for N days (0 = all-time). Must complete successfully before reading global stats/history.",
+			  Keywords="steam stats global aggregated request fetch cache history"
+	))
+	static USAL_RequestGlobalStats* RequestGlobalStats(int32 Days = 0);
+
+	UPROPERTY(BlueprintAssignable, Category="SteamSAL|Stats")
+	FSAL_RequestGlobalStatsSuccess OnSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category="SteamSAL|Stats")
+	FSAL_RequestGlobalStatsFailure OnFailure;
+
+	virtual void Activate() override;
+
+private:
+	int32 RequestedDays = 0;
+
+	CCallResult<USAL_RequestGlobalStats, GlobalStatsReceived_t> CallResult;
+
+	void OnGlobalStatsReceived(GlobalStatsReceived_t* Callback, bool bIOFailure);
+
+};
